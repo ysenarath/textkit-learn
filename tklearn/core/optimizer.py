575afe033e2a91f464db75bd5fa106b4c1e394df
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2023-10-24 20:45:50 -0400

    Minor updates to optimizer.

--- tklearn/core/optimizer.py
+++ tklearn/core/optimizer.py
@@ -1,11 +1,32 @@
-from typing import Callable
+import abc
 import optuna
 from optuna import Study
+from typing import Callable
+
+__all__ = [
+    "Optimizer",
+]
 
 
-class Optimizer(object):
-    def __init__(self, study: Study) -> None:
-        self._study = study
+class Optimizer(abc.ABC):
+    @abc.abstractmethod
+    def objective(self, *args, **kwargs):
+        pass
 
-    def run(self, n_trials: int = 100) -> None:
-        self._study.optimize(self._objective, n_trials=n_trials)
+    def optimize(self, n_trials: int = 100) -> None:
+        study = optuna.create_study()
+        study.optimize(self.objective, n_trials=n_trials)
+        return study
+
+
+class OptimizerFunction(Optimizer):
+    def __init__(self, func: Callable) -> None:
+        super(OptimizerFunction, self).__init__()
+        self.func = func
+
+    def objective(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+def optimizer(func: Callable):
+    return OptimizerFunction(func)
