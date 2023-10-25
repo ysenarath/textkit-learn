fdcb28d52690d46226d7870e66db9c552feacdd7
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2023-10-24 20:46:19 -0400

    Update tracking with mlflow.

--- tklearn/core/tracking.py
+++ tklearn/core/tracking.py
@@ -1,13 +1,83 @@
 from __future__ import annotations
-from typing import Any, Optional, Union
+from collections.abc import Mapping
+from typing import Any, Optional, Union, Dict
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import mlflow
+from matplotlib.figure import Figure
+from plotly import graph_objects as go
 from mlflow.entities import (
     Experiment as MLflowExperiment,
     Run as MLflowRun,
 )
 
+__all__ = [
+    "Run",
+    "Experiment",
+]
 
-class Run:
+
+def flatten(data: Dict[str, Any], parent_key="", separator="__"):
+    items = []
+    for key, value in data.items():
+        # escape dots
+        new_key = parent_key + separator + key if parent_key else key
+        if isinstance(value, Mapping):
+            items.extend(
+                flatten(
+                    value,
+                    parent_key=new_key,
+                    separator=separator,
+                ).items()
+            )
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+import enum
+
+
+class ArtifactType(enum.Enum):
+    PATH = 0
+    JSON = 1
+    TABLE = 2
+    FIGURE = 3
+    IMAGE = 4
+    HTML = 5
+    TEXT = 6
+
+
+class Artifact(object):
+    def __init__(self, obj, type: Optional[ArtifactType] = None) -> None:
+        self.obj = obj
+        if type is None:
+            try:
+                objp = Path(obj).exists()
+            except Exception as ex:
+                objp = False
+            if isinstance(obj, Path) or objp:
+                type = ArtifactType.PATH
+            elif isinstance(obj, str):
+                type = ArtifactType.TEXT
+            elif isinstance(obj, dict):
+                type = ArtifactType.JSON
+            elif isinstance(obj, pd.DataFrame):
+                type = ArtifactType.TABLE
+            elif isinstance(obj, (Figure, go.Figure)):
+                type = ArtifactType.FIGURE
+            elif isinstance(obj, (np.ndarray,)):
+                type = ArtifactType.IMAGE
+            elif isinstance(obj, (np.ndarray,)):
+                type = ArtifactType.IMAGE
+            else:
+                type = None
+        self.type = type
+
+
+class Run(object):
     def __init__(self, experiment: Experiment, mlflow_run: MLflowRun) -> None:
         self.experiment = experiment
         self._mlflow_run: MLflowRun = mlflow_run
@@ -33,15 +103,16 @@ class Run:
     def log_metric(
         self,
         key: str,
-        value: float,
+        value: Any,
         *,
         timestamp: Optional[int] = None,
         step: Optional[int] = None,
     ) -> None:
         """Log a metric to MLFlow."""
-        self._mlflow_client.log_metric(
-            self._mlflow_run.info.run_id, key, value, timestamp, step
-        )
+        for key, value in flatten({key: value}).items():
+            self._mlflow_client.log_metric(
+                self._mlflow_run.info.run_id, key, value, timestamp, step
+            )
 
     def log_metrics(
         self,
@@ -54,9 +125,14 @@ class Run:
         for key, value in metrics.items():
             self.log_metric(key, value, timestamp=timestamp, step=step)
 
-    def log_param(self, key: str, value: str) -> None:
+    def log_param(self, key: str, value: Any) -> None:
         """Log a parameter to MLFlow."""
-        self._mlflow_client.log_param(self._mlflow_run.info.run_id, key, value)
+        for key, value in flatten({key: value}).items():
+            self._mlflow_client.log_param(
+                self._mlflow_run.info.run_id,
+                key,
+                value,
+            )
 
     def log_params(self, params: dict) -> None:
         """Log parameters to MLFlow."""
@@ -72,7 +148,51 @@ class Run:
         for key, value in tags.items():
             self.set_tag(key, value)
 
-    def terminate(self) -> None:
+    def log_artifact(
+        self,
+        artifact: Union[Artifact, Any],
+        artifact_path: Optional[str] = None,
+        artifact_type: Optional[ArtifactType] = None,
+    ):
+        if not isinstance(artifact, Artifact):
+            artifact = Artifact(artifact, type=artifact_type)
+        obj = artifact.obj
+        if artifact.type == ArtifactType.PATH:
+            self._mlflow_client.log_artifact(
+                self._mlflow_run.info.run_id, obj, artifact_path=artifact_path
+            )
+        elif artifact.type == ArtifactType.JSON:
+            self._mlflow_client.log_dict(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        elif artifact.type == ArtifactType.TABLE:
+            self._mlflow_client.log_table(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        elif artifact.type == ArtifactType.FIGURE:
+            self._mlflow_client.log_figure(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        elif artifact.type == ArtifactType.IMAGE:
+            self._mlflow_client.log_image(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        elif artifact.type == ArtifactType.HTML:
+            if isinstance(obj, pd.DataFrame):
+                obj = obj.to_html()
+            self._mlflow_client.log_text(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        elif artifact.type == ArtifactType.TEXT:
+            if isinstance(obj, pd.DataFrame):
+                obj = obj.to_html()
+            self._mlflow_client.log_text(
+                self._mlflow_run.info.run_id, obj, artifact_file=artifact_path
+            )
+        else:
+            raise NotImplementedError(f"unsupported artifact type '{artifact.type}'")
+
+    def set_terminated(self) -> None:
         """Close the run."""
         self._mlflow_client.set_terminated(self._mlflow_run.info.run_id)
 
@@ -91,22 +211,38 @@ class Experiment(object):
 
     def __init__(
         self,
+        name: Union[str, None] = None,
         tracking_uri: Optional[str] = None,
         registry_uri: Optional[str] = None,
         artifact_location: Optional[str] = None,
-        experiment_name: Union[str, None] = None,
     ) -> None:
         self._mlflow_client = mlflow.tracking.MlflowClient(
             tracking_uri=tracking_uri,
             registry_uri=registry_uri,
         )
-        self._mlflow_client._get_registry_client()
+        # self._mlflow_client._get_registry_client()
         self._mlflow_experiment: MLflowExperiment = (
             self._mlflow_get_or_create_experiment(
-                experiment_name,
+                name,
                 artifact_location=artifact_location,
             )
         )
+
+    @property
+    def name(self):
+        return self._mlflow_experiment.name
+
+    @property
+    def tracking_uri(self) -> str:
+        return self._mlflow_client.tracking_uri
+
+    @property
+    def registry_uri(self) -> str:
+        return self._mlflow_client._registry_uri
+
+    @property
+    def artifact_location(self):
+        return self._mlflow_experiment.artifact_location
 
     def _mlflow_get_or_create_experiment(
         self,
@@ -116,12 +252,13 @@ class Experiment(object):
         """Get or create an MLFlow experiment."""
         if experiment_name is None:
             experiment_name = "Default"
-        experiment = self._mlflow_client.get_experiment_by_name(experiment_name)
-        if experiment is None:
+        try:
             experiment_id = self._mlflow_client.create_experiment(
                 experiment_name, artifact_location=artifact_location
             )
             experiment = self._mlflow_client.get_experiment(experiment_id)
+        except:
+            experiment = self._mlflow_client.get_experiment_by_name(experiment_name)
         return experiment
 
     @property
