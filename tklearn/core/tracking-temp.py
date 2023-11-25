34f92f851dd71e6ff99b8e8c3f584cef57eab6bd
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2023-11-24 20:09:09 -0500

    Backup old tracking.

--- tklearn/core/tracking-temp.py
+++ tklearn/core/tracking-temp.py
@@ -1,11 +1,25 @@
 from __future__ import annotations
 from ast import Tuple
+import enum
+from collections.abc import Mapping
+import contextlib
+from datetime import datetime
+import errno
 import hashlib
-from typing import Any, Dict, Mapping, Optional, Union
+import json
+import math
+import os
+from typing import Any, Optional, Union, Dict
 from pathlib import Path
 import uuid
+import filelock
+from matplotlib import pyplot as plt
 
-import tensorflow as tf
+import numpy as np
+import pandas as pd
+from matplotlib.figure import Figure
+from plotly import graph_objects as go
+
 
 __all__ = [
     "Run",
@@ -14,11 +28,7 @@ __all__ = [
 ]
 
 
-def flatten(
-    data: Dict[str, Any],
-    parent_key: str = "",
-    separator: str = ".",
-):
+def flatten(data: Dict[str, Any], parent_key="", separator="."):
     items = []
     for key, value in data.items():
         # escape dots
@@ -36,6 +46,119 @@ def flatten(
     return dict(items)
 
 
+def create_dir_if_not_exist(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            # File exists, and it's a directory,
+            # another process beat us to creating this dir, that's OK.
+            pass
+        else:
+            # Our target dir exists as a file, or different error,
+            # reraise the error!
+            raise
+
+
+def explore_recursively(
+    run_path: Path,
+    namespace: str = "",
+    base_run_id: Optional[str] = None,
+    parameters: Optional[list] = None,
+):
+    run_id = str(run_path).split("/")[-1]
+    if base_run_id is None:
+        base_run_id = run_id
+    subpath = run_path / "metadata.json"
+    with open(subpath, "r", encoding="utf-8") as fp:
+        metadata = json.load(fp)
+        run_name = metadata["name"]
+    namespace = namespace + "." + run_name if namespace else run_name
+    if parameters is None:
+        parameters = []
+    for file in os.listdir(run_path):
+        subpath = run_path / file
+        if file == "metadata.json":
+            continue
+        if not file.endswith(".json"):
+            continue
+        with open(subpath, "r", encoding="utf-8") as fp:
+            d = json.load(fp)
+        if d["type"] != "parameter":
+            continue
+        d["metadata.base_run.id"] = base_run_id
+        d["metadata.run.id"] = run_id
+        d["metadata.run.namespace"] = namespace
+        parameters.append(d)
+    data = []
+    for file in os.listdir(run_path):
+        subpath = run_path / file
+        if file == "metadata.json":
+            continue
+        if subpath.is_dir() and file.startswith("run-"):
+            data += explore_recursively(
+                subpath,
+                namespace=namespace,
+                base_run_id=base_run_id,
+                parameters=parameters.copy(),
+            )
+            continue
+        if not file.endswith(".json"):
+            continue
+        with open(subpath, "r", encoding="utf-8") as fp:
+            d = json.load(fp)
+        if d["type"] == "parameter":
+            continue
+        d["metadata.base_run.id"] = base_run_id
+        d["metadata.run.id"] = run_id
+        d["metadata.run.namespace"] = namespace
+        d["parameters"] = [
+            p
+            for p in parameters
+            if p["step"] is None
+            or (p["step"] == d["step"] and d["metadata.run.id"] == p["metadata.run.id"])
+        ]
+        data.append(d)
+    return data
+
+
+class ArtifactType(enum.Enum):
+    PATH = 0
+    JSON = 1
+    TABLE = 2
+    FIGURE = 3
+    IMAGE = 4
+    HTML = 5
+    TEXT = 6
+
+
+class Artifact(object):
+    def __init__(self, obj, type: Optional[ArtifactType] = None) -> None:
+        self.obj = obj
+        if type is None:
+            try:
+                objp = Path(obj).exists()
+            except Exception as ex:
+                objp = False
+            if isinstance(obj, Path) or objp:
+                type = ArtifactType.PATH
+            elif isinstance(obj, str):
+                type = ArtifactType.TEXT
+            elif isinstance(obj, dict):
+                type = ArtifactType.JSON
+            elif isinstance(obj, pd.DataFrame):
+                type = ArtifactType.TABLE
+            elif isinstance(obj, (Figure, go.Figure)):
+                type = ArtifactType.FIGURE
+            elif isinstance(obj, (np.ndarray,)):
+                type = ArtifactType.IMAGE
+            elif isinstance(obj, (np.ndarray,)):
+                type = ArtifactType.IMAGE
+            else:
+                type = None
+        self.type = type
+
+
 class Run(object):
     def __init__(
         self,
@@ -49,6 +172,19 @@ class Run(object):
         if id is None:
             self.id = f"run-{uuid.uuid4().hex}"
             name = "default" if name is None else name
+            create_dir_if_not_exist(self.run_path)
+            with self.experiment.lock():
+                with open(
+                    self.run_path / "metadata.json",
+                    "w",
+                    encoding="utf-8",
+                ) as fp:
+                    json.dump(
+                        {
+                            "name": name,
+                        },
+                        fp,
+                    )
         else:
             self.id = id
 
@@ -59,57 +195,69 @@ class Run(object):
             path = path / parent_id
         return path / self.id
 
-    @property
-    def writer(self) -> tf.summary.SummaryWriter:
-        if not hasattr(self, "_writer") or self._writer is None:
-            self._writer = tf.summary.create_file_writer(str(self.run_path))
-        return self._writer
+    def _log_data(self, **kwargs):
+        if "timestamp" not in kwargs or kwargs["timestamp"] is None:
+            kwargs["timestamp"] = math.ceil(datetime.now().timestamp())
+        with self.experiment.lock():
+            path = self.run_path / f"{uuid.uuid4().hex}.json"
+            with open(path, "w", encoding="utf-8") as fp:
+                json.dump(kwargs, fp)
+
+    def get_logs(self) -> dict:
+        return explore_recursively(self.run_path)
 
     def log_metric(
         self,
         key: str,
         value: Any,
         *,
-        step: Optional[int] = 0,
+        timestamp: Optional[int] = None,
+        step: Optional[int] = None,
     ) -> None:
         """Log a metric to MLFlow."""
         for key, value in flatten({key: value}).items():
-            with self.writer.as_default():
-                tf.summary.scalar(f"metric.{key}", value, step=step)
-            self.writer.flush()
+            self._log_data(
+                type="metric",
+                key=key,
+                value=value,
+                step=step,
+                timestamp=timestamp,
+            )
 
     def log_param(
         self,
         key: str,
         value: Any,
-        step: Optional[int] = 0,
+        step: Optional[int] = None,
     ) -> None:
         """Log a parameter to MLFlow."""
         for key, value in flatten({key: value}).items():
-            with self.writer.as_default():
-                tf.summary.scalar(f"param.{key}", value, step=step)
-            self.writer.flush()
+            self._log_data(
+                type="parameter",
+                key=key,
+                value=value,
+                step=step,
+            )
 
-    def set_tag(
-        self,
-        key: str,
-        value: Any,
-    ) -> None:
+    def set_tag(self, key: str, value: Any) -> None:
         """Set tags for the run."""
         for key, value in flatten({key: value}).items():
-            with self.writer.as_default():
-                tf.summary.scalar(f"tag.{key}", value, step=0)
-            self.writer.flush()
+            self._log_data(
+                type="tag",
+                key=key,
+                value=value,
+            )
 
     def log_metrics(
         self,
         metrics: dict,
         *,
+        timestamp: Optional[int] = None,
         step: Optional[int] = None,
     ) -> None:
         """Log metrics to MLFlow."""
         for key, value in metrics.items():
-            self.log_metric(key, value, step=step)
+            self.log_metric(key, value, timestamp=timestamp, step=step)
 
     def log_params(
         self,
@@ -125,22 +273,20 @@ class Run(object):
         for key, value in tags.items():
             self.set_tag(key, value)
 
-    def start_run(
+    def log_artifact(
         self,
-        run_name: Optional[str] = None,
+        artifact: Union[Artifact, Any],
+        artifact_path: Optional[str] = None,
+        artifact_type: Optional[ArtifactType] = None,
     ):
+        # if not isinstance(artifact, Artifact):
+        #     artifact = Artifact(artifact, type=artifact_type)
+        # obj = artifact.obj
+        raise NotImplementedError
+
+    def start_run(self, run_name: Optional[str] = None):
         """Start a new run."""
         return Run(self.experiment, run_name, parent_ids=self.parent_ids + (self.id,))
-
-    def log_text(
-        self,
-        name: str,
-        data: str,
-        step: Optional[int] = None,
-    ):
-        with self.writer.as_default():
-            tf.summary.text(name, data, step=step)
-        self.writer.flush()
 
 
 class Experiment(object):
@@ -150,16 +296,108 @@ class Experiment(object):
         self,
         path: Union[str, Path],
         name: Union[str, None] = None,
+        version: Optional[str] = None,
     ) -> None:
         if name is None:
             name = "main"
         self.path = Path(path)
         self.experiment_id = hashlib.sha256(name.encode()).hexdigest()
+        create_dir_if_not_exist(self.tracking_uri)
+        with self.lock():
+            with open(
+                self.tracking_uri / "metadata.json",
+                "w",
+                encoding="utf-8",
+            ) as fp:
+                json.dump(
+                    {
+                        "name": name,
+                        "version": version,
+                    },
+                    fp,
+                )
 
     @property
     def tracking_uri(self) -> Path:
         return self.path / f"expr-{self.experiment_id}"
 
-    def start_run(self, run_name: str):
+    def start_run(self, run_name: Optional[str] = None):
         """Start a new run."""
         return Run(self, run_name)
+
+    @contextlib.contextmanager
+    def lock(self):
+        yield filelock.FileLock(self.tracking_uri / "experiment.lock")
+
+    def start_run(self, run_name: Optional[str] = None):
+        """Start a new run."""
+        return Run(self, run_name)
+
+    def list_runs(self):
+        return [
+            Run(self, id=run_id)
+            for run_id in os.listdir(self.tracking_uri)
+            if run_id.startswith("run-")
+        ]
+
+
+class StatType:
+    def __init__(
+        self,
+        values: np.array,
+    ):
+        self.values = values
+        self.mean = np.mean(values)
+        self.std = np.std(values)
+
+    def __repr__(self):
+        return "{:0.2f}±{:0.2f}".format(self.mean, self.std)
+
+
+@pd.api.extensions.register_dataframe_accessor("stat")
+class AggAccessor:
+    def __init__(self, df: pd.DataFrame):
+        self._obj: Union[pd.DataFrame, pd.Series] = (
+            df[df["type"] == "metric"]
+            .pivot_table(
+                values="value",
+                index="step",
+                columns="key",
+                aggfunc=StatType,
+            )
+            .sort_index()
+        )
+
+    def __getitem__(self, name):
+        new = AggAccessor.__new__(AggAccessor)
+        new._obj = self._obj[name]
+        return new
+
+    @property
+    def df(self) -> Union[pd.DataFrame, pd.Series]:
+        return self._obj
+
+    @property
+    def index(self):
+        # get the means of the dataframe/series
+        return self._obj.index
+
+    @property
+    def mean(self):
+        # get the means of the dataframe/series
+        return self._obj.apply(lambda x: x.mean)
+
+    @property
+    def std(self):
+        # get the std of the dataframe/series
+        return self._obj.apply(lambda x: x.std)
+
+    def plot(self):
+        plt.errorbar(
+            self.index,
+            self.mean,
+            self.std,
+            linestyle="None",
+            marker="o",
+        )
+        plt.show()
