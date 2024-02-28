743e5ea88c9eca26f2d3b9e3bb382377398e5fae
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-27 20:10:46 -0500

    Delete test_hf_tok.py and test_trainer.py examples

--- examples/test_hf_tok.py
+++
@@ -1,7 +0,0 @@
-from tklearn.preprocessing.tokenization import HuggingFaceTokenizer
-
-tokenizer = HuggingFaceTokenizer.from_pretrained("bert-base-uncased")
-
-enc = tokenizer.tokenize("Hello, world!", return_tensors="pt")
-
-print(enc["input_ids"])
