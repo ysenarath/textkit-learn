388b229c3df29e6369a47a194a44e90e46fe0ab3
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-03-18 20:38:59 -0400

    Add new callbacks

--- tklearn/nn/callbacks/__init__.py
+++ tklearn/nn/callbacks/__init__.py
@@ -2,10 +2,16 @@ from tklearn.nn.callbacks.base import (
     ModelCallback,
     ModelCallbackList,
 )
+from tklearn.nn.callbacks.checkpoint import ModelCheckpoint
+from tklearn.nn.callbacks.early_stopping import EarlyStopping
 from tklearn.nn.callbacks.progbar_logger import ProgbarLogger
+from tklearn.nn.callbacks.tracking import TrackingCallback
 
 __all__ = [
+    "EarlyStopping",
     "ModelCallback",
     "ModelCallbackList",
+    "ModelCheckpoint",
     "ProgbarLogger",
+    "TrackingCallback",
 ]
