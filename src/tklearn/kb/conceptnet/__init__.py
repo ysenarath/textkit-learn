9cf30ac8801b8b00ac9c6b4db655cce16cb02023
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-11 12:13:25 -0400

    Add ConceptNetIO import to __init__.py

--- src/tklearn/kb/conceptnet/__init__.py
+++ src/tklearn/kb/conceptnet/__init__.py
@@ -1,8 +1,10 @@
 from tklearn.kb.conceptnet import concept, io, relation, uri
+from tklearn.kb.conceptnet.io import ConceptNetIO
 
 __all__ = [
     "uri",
     "concept",
     "relation",
     "io",
+    "ConceptNetIO",
 ]
