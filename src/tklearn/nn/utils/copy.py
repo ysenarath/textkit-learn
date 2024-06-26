79251608f3646965cb8f0b1a11ef0802b9accde9
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-26 00:44:25 -0400

    Refactor deepcopy function to handle different types of objects

--- src/tklearn/nn/utils/copy.py
+++ src/tklearn/nn/utils/copy.py
@@ -1,40 +1,33 @@
 import copy
-from typing import TYPE_CHECKING, Any, Mapping, Union
+from typing import Any, Mapping, TypeVar, Union
 
 import torch
 
 from tklearn.nn.utils.array import move_to_device
 
-if TYPE_CHECKING:
-    from tklearn import nn
+T = TypeVar("T", bound=Any)
 
 
-def _get_device(model: Any) -> torch.device:
-    if isinstance(model, nn.Module):
-        return model.device
-    elif isinstance(model, Mapping):
-        return _get_device(next(iter(model.values())))
-    msg = (
-        "model must be either a nn.Module or a Mapping,"
-        f" got {model.__class__.__name__}"
-    )
+def _get_device(x: Any) -> torch.device:
+    if hasattr(x, "device"):
+        return x.device
+    elif isinstance(x, Mapping):
+        return _get_device(next(iter(x.values())))
+    msg = f"unable determine device for object of type {x.__class__.__name__}"
     raise TypeError(msg)
 
 
-def deepcopy(
-    model: Union[nn.Module, Mapping[str, torch.Tensor]],
-    device: Union[str, torch.device, None] = None,
-) -> nn.Module:
+def deepcopy(x: T, device: Union[str, torch.device, None] = None) -> T:
     """Deep copy a model."""
-    current_device = _get_device(model)
+    current_device = _get_device(x)
     if device is None:
         device = current_device
     elif isinstance(device, str):
         device = torch.device(device)
     if device != current_device:
-        move_to_device(model, device)
-        model_copy = copy.deepcopy(model)
-        move_to_device(model, current_device)
+        move_to_device(x, device)
+        model_copy = copy.deepcopy(x)
+        move_to_device(x, current_device)
     else:
-        model_copy = copy.deepcopy(model)
+        model_copy = copy.deepcopy(x)
     return model_copy
