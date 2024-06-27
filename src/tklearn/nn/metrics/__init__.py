9ca95b8ad3b70f7ef409ccaee639c3b53b884a23
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-27 16:01:44 -0400

    Add nuanced_bias_report and mcnemar to metrics package

--- src/tklearn/nn/metrics/__init__.py
+++ src/tklearn/nn/metrics/__init__.py
@@ -1,6 +1,10 @@
 from tklearn.nn.metrics.base import Evaluator, Metric
+from tklearn.nn.metrics.bias import nuanced_bias_report
+from tklearn.nn.metrics.statistical_tests import mcnemar
 
 __all__ = [
     "Evaluator",
     "Metric",
+    "mcnemar",
+    "nuanced_bias_report",
 ]
