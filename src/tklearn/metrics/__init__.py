837bc23d1a01cb959cef8a4f07ed9402b0f22efa
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-29 20:55:14 -0400

    Add classification metrics and common functions

--- src/tklearn/metrics/__init__.py
+++ src/tklearn/metrics/__init__.py
@@ -1,5 +1,15 @@
 from tklearn.metrics.base import Evaluator, Metric
 from tklearn.metrics.bias import nuanced_bias_report
+from tklearn.metrics.classification import (
+    Accuracy,
+    F1Score,
+    Precision,
+    Recall,
+)
+from tklearn.metrics.common import (
+    y_pred_getter,
+    y_score_getter,
+)
 from tklearn.metrics.statistical_tests import mcnemar
 
 __all__ = [
@@ -7,4 +17,11 @@ __all__ = [
     "Metric",
     "mcnemar",
     "nuanced_bias_report",
+    # Metrics
+    "Accuracy",
+    "Precision",
+    "Recall",
+    "F1Score",
+    "y_pred_getter",
+    "y_score_getter",
 ]
