f2a9a26539e953788e4f2ac1ab5da28544ae273f
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-12-19 23:39:43 -0500

    Remove unused utility functions and obsolete classes from the codebase

--- src/tklearn/config.py
+++ src/tklearn/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from pathlib import Path
 
 from omegaconf import OmegaConf
@@ -7,32 +7,8 @@ DEFAULT_CACHE_PATH: str = str((Path.home() / ".cache" / "tklearn").absolute())
 
 
 @dataclass
-class ConceptNetConfig:
-    download_url: str = "${{oc.env:{env_var},{url}}}".format(
-        env_var="TKLEARN_CONCEPTNET_DOWNLOAD_URL",
-        url="https://s3.amazonaws.com/conceptnet/downloads/2019/edges/conceptnet-assertions-5.7.0.csv.gz",
-    )
-
-
-@dataclass
-class NRCConfig:
-    # National Research Council Canada (NRC)
-    emonet_download_url: str = "${{oc.env:{env_var},{url}}}".format(
-        env_var="TKLEARN_NRC_EMONET_DOWNLOAD_URL",
-        url="https://saifmohammad.com/WebDocs/Lexicons/NRC-Emotion-Lexicon.zip",
-    )
-
-
-@dataclass
-class ExternalConfig:
-    conceptnet: ConceptNetConfig = field(default_factory=ConceptNetConfig)
-    nrc: NRCConfig = field(default_factory=NRCConfig)
-
-
-@dataclass
 class Config:
     cache_dir: Path = f"${{oc.env:TKLEARN_CACHE,{DEFAULT_CACHE_PATH}}}"
-    external: ExternalConfig = field(default_factory=ExternalConfig)
 
 
 config: Config = OmegaConf.structured(Config)
--- src/tklearn/utils/__init__.py
+++ src/tklearn/utils/__init__.py
@@ -1,8 +1,6 @@
 from tklearn.utils.hashing import get_content_hash
-from tklearn.utils.io import download
 
 __all__ = [
-    "download",
     "get_content_hash",
     "KeywordProcessor",
 ]
