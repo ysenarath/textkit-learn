43a5d5a8e030ed61cee14d5c074a8000899935c0
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-01-03 11:56:30 -0500

    Refactor DatasetMapper and add load_dataset and islice functions for enhanced dataset handling

--- src/tklearn/utils/datasets.py
+++ src/tklearn/utils/datasets.py
@@ -1,34 +1,41 @@
 from __future__ import annotations
 
+import functools
 import tempfile
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Union
+from typing import Any, Callable, Dict, Generator, List, Union
 
 import pandas as pd
-from datasets import Dataset, DatasetDict
+from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict
+from datasets import load_dataset as hf_load_dataset
 from tqdm import auto as tqdm
 
 from tklearn.config import config
+from tklearn.core.document import Document
 
 T_BI = Dict[str, List[Any]]
 T_BO = Union[Dict[str, List[Any]], List[Dict[str, Any]], pd.DataFrame]
 T_I = Dict[str, Any]
 T_O = Dict[str, Any]
 
+CACHE_DIR = Path.home().absolute() / ".cache" / "tklearn"
+
 
 class DatasetMapper:
     def __init__(
         self,
         func: Callable[[T_BI], T_BO],
-        batched: bool = True,
         *,
-        cache_dir: Path | str | None = None,
+        batched: bool = True,
+        temp_dir: Path | str | None = None,
     ):
-        if isinstance(cache_dir, str):
-            cache_dir = Path(cache_dir)
-        elif cache_dir is None:
-            cache_dir = Path(config.cache_dir) / "cache"
-        self._cache_dir = cache_dir
+        if isinstance(temp_dir, str):
+            temp_dir = Path(temp_dir)
+        elif temp_dir is None and config is not None:
+            temp_dir = Path(config.cache_dir) / "temp"
+        else:
+            temp_dir = CACHE_DIR / "temp"
+        self._temp_dir = temp_dir
         self.func = func
         self.batched = batched
 
@@ -36,10 +43,10 @@ class DatasetMapper:
         return self.func(*args, **kwargs)
 
     @property
-    def cache_dir(self) -> Path:
-        if not self._cache_dir.exists():
-            self._cache_dir.mkdir(parents=True)
-        return self._cache_dir
+    def temp_dir(self) -> Path:
+        if not self._temp_dir.exists():
+            self._temp_dir.mkdir(parents=True)
+        return self._temp_dir
 
     def map(
         self,
@@ -89,7 +96,7 @@ class DatasetMapper:
             func_kwargs = {}
         nlen = len(dataset)
         output = None
-        with tempfile.TemporaryDirectory(dir=self.cache_dir) as tmp_dir:
+        with tempfile.TemporaryDirectory(dir=self.temp_dir) as tmp_dir:
             outpaths = []
             for i in tqdm.trange(0, nlen, batch_size, disable=not verbose):
                 batch = dataset[i : i + batch_size]
@@ -108,6 +115,28 @@ class DatasetMapper:
         return output
 
 
+@functools.wraps(hf_load_dataset)
+def load_dataset(
+    *args, **kwargs
+) -> DatasetDict | Dataset | IterableDatasetDict | IterableDataset:
+    return hf_load_dataset(*args, **kwargs)
+
+
+def islice(
+    dataset: Dataset | DatasetDict, *args, **kwargs
+) -> Generator[Document, None, None]:
+    split = kwargs.get("split", None)
+    if split is None:
+        n = dataset.num_rows
+    else:
+        n = dataset[split].num_rows
+    for i in range(*args):
+        if i >= n:
+            break
+        j = n + i if i < 0 else i
+        yield Document.from_dataset(dataset, j, split)
+
+
 def map_dataset(
     dataset: Dataset | DatasetDict | T_BO,
     func: Callable[[T_BI], T_BO],
@@ -116,9 +145,9 @@ def map_dataset(
     func_kwargs: dict | None = None,
     verbose: bool = False,
     *,
-    cache_dir: Path | str | None = None,
+    temp_dir: Path | str | None = None,
 ) -> Dataset:
-    mapper = DatasetMapper(func, batched, cache_dir=cache_dir)
+    mapper = DatasetMapper(func, batched=batched, temp_dir=temp_dir)
     return mapper.map(
         dataset,
         batch_size=batch_size,
