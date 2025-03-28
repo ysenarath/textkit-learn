b5fad87b785a98c736e952d947891792bb077458
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-03-27 22:39:58 -0400

    Add run_id parameter to TrackingCallback for improved MLflow logging

--- src/tklearn/nn/callbacks/tracking.py
+++ src/tklearn/nn/callbacks/tracking.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, Union
 
 import mlflow
+from mlflow.tracking.fluent import ActiveRun
 
 from tklearn.nn.callbacks.base import Callback
 
@@ -11,7 +12,10 @@ __all__ = [
 
 class TrackingCallback(Callback):
     def __init__(
-        self, prefix: Optional[str] = None, exclude: Optional[str] = None
+        self,
+        prefix: Optional[str] = None,
+        exclude: Optional[str] = None,
+        run: Union[ActiveRun, str, None] = None,
     ):
         super().__init__()
         # the actual run used to track the trainer progress
@@ -19,6 +23,12 @@ class TrackingCallback(Callback):
             prefix = ""
         self.prefix = str(prefix).strip()
         self.exclude = exclude
+        if isinstance(run, str):
+            self.run_id = run
+        elif run is not None:
+            self.run_id = run.info.run_id
+        else:
+            self.run_id = None
 
     @property
     def exclude(self) -> set:
@@ -33,14 +43,16 @@ class TrackingCallback(Callback):
         self._exclude = set(values)
 
     def on_epoch_end(self, epoch: int, logs: Optional[dict] = None):
-        mlflow.log_metric(f"{self.prefix}epoch", epoch, step=epoch)
+        mlflow.log_metric(
+            f"{self.prefix}epoch", epoch, step=epoch, run_id=self.run_id
+        )
         logs = {
             f"{self.prefix}{k}": v
             for k, v in logs.items()
             if self.is_loggable(k, v)
         }
         # metrics must be a Dict[str, float]
-        mlflow.log_metrics(logs, step=epoch)
+        mlflow.log_metrics(logs, step=epoch, run_id=self.run_id)
 
     def is_loggable(self, key: str, value) -> bool:
         return key not in self.exclude and isinstance(value, (int, float))
