b870f7c8cfc9286dd5f281246865dd2cea056ea6
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-04-27 13:03:04 -0400

    Minor updates to the example code

--- examples/tklearn-knowledge-base.py
+++ examples/tklearn-knowledge-base.py
@@ -6,9 +6,9 @@ from tklearn.kb import KnowledgeBase
 def test_pickle():
     kb = KnowledgeBase("wiktionary")
     # Test pickling and unpickling the KnowledgeBase
-    kb2 = pickle.loads(pickle.dumps(kb))
+    pickle.loads(pickle.dumps(kb))
     # Test pickling and unpickling the KnowledgeBase with a different protocol
-    kb2 = pickle.loads(pickle.dumps(kb, protocol=pickle.HIGHEST_PROTOCOL))
+    pickle.loads(pickle.dumps(kb, protocol=pickle.HIGHEST_PROTOCOL))
 
 
 if __name__ == "__main__":
