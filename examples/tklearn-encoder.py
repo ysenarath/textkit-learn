242193c06bfda87c65192b14ee744d5918fafa75
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-04-27 13:03:16 -0400

    Add example script for using Encoder with BERT model on Yelp dataset

---
+++ examples/tklearn-encoder.py
@@ -0,0 +1,55 @@
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+from tklearn.nn import Encoder
+from tklearn.nn.callbacks import ProgbarLogger
+from tklearn.nn.models import AutoModel
+from tklearn.nn.utils import get_device
+
+MODEL_NAME_OR_PATH = "google-bert/bert-base-uncased"
+DATASET = "yelp_review_full"
+
+model = AutoModel({
+    "type": "linear",
+    "backbone": {
+        "type": "transformer",
+        "model_name_or_path": MODEL_NAME_OR_PATH,
+    },
+    "num_labels": 5,
+})
+
+auto_device = get_device()
+
+model.to(auto_device)
+
+dataset = load_dataset(DATASET, split="train")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH)
+
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], truncation=True, padding="max_length")
+
+
+dataset = dataset.rename_column("label", "labels").map(
+    tokenize_function, batched=True, remove_columns=["text"]
+)
+
+dataset.set_format(type="torch")
+
+dataloader = DataLoader(
+    dataset,
+    batch_size=32,
+    pin_memory=True,
+)
+
+evaluator = Encoder(
+    model,
+    dataloader=dataloader,
+    callbacks=[ProgbarLogger()],
+)
+
+encoded = evaluator.encode()
+
+print(encoded)
