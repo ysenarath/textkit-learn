4291ab738f325b973d2ea7f76a040e0ec566a746
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-04-27 12:39:21 -0400

    Add Encoder class for model predictions and data encoding

--- src/tklearn/nn/base/__init__.py
+++ src/tklearn/nn/base/__init__.py
@@ -1,9 +1,11 @@
+from tklearn.nn.base.encoder import Encoder
 from tklearn.nn.base.evaluator import Evaluator
 from tklearn.nn.base.module import Module
 from tklearn.nn.base.predictor import Predictor
 from tklearn.nn.base.trainer import Trainer
 
 __all__ = [
+    "Encoder",
     "Module",
     "Predictor",
     "Evaluator",
---
+++ src/tklearn/nn/base/encoder.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from collections.abc import Iterable, Mapping
+from typing import Generator, Generic, TypeVar, Union
+
+import torch
+from torch.utils.data import DataLoader
+
+from tklearn.nn.base.module import Module
+from tklearn.nn.callbacks.base import Callback, CallbackList, CallbacksMixin
+from tklearn.nn.loss import LossDict
+from tklearn.utils.array import concat, move_to_device
+
+K = TypeVar("K")
+V = TypeVar("V")
+L = Union[torch.Tensor, Mapping[str, torch.Tensor], LossDict, None]
+
+
+class Encoder(CallbacksMixin, Generic[K, V]):
+    def __init__(
+        self,
+        model: Module[K, V],
+        dataloader: DataLoader,
+        callbacks: CallbackList | Iterable[Callback] | None = None,
+    ) -> None:
+        super().__init__()
+        self.model = model
+        self.dataloader = dataloader
+        self.callbacks = callbacks
+
+    @torch.no_grad()
+    def iter_batches(
+        self,
+    ) -> Generator[tuple[int, K, V, LossDict], None, None]:
+        if self.model.training:
+            self.model.eval()
+
+        # set the callback params
+        callback_params = {}
+
+        if self.callbacks.params is not None:
+            callback_params.update(self.callbacks.params)
+
+        callback_params.update({"pred_steps": len(self.dataloader)})
+
+        self.callbacks.set_params(callback_params)
+        self.callbacks.set_model(self.model)
+
+        # start the prediction
+        self.callbacks.on_predict_begin()
+
+        dataloader_idx = None
+        for batch_idx, batch in enumerate(self.dataloader):
+            batch = move_to_device(batch, self.model.device, non_blocking=True)
+            self.callbacks.on_predict_batch_begin(batch_idx)
+            output = self.model.predict_step(
+                batch, batch_idx=batch_idx, dataloader_idx=dataloader_idx
+            )
+            self.callbacks.on_predict_batch_end(batch_idx, logs={})
+            yield batch_idx, batch, output, None
+
+        self.callbacks.on_predict_end()
+
+    def encode(self) -> torch.Tensor:
+        self.model.eval()
+
+        encoding = None
+
+        for batch_idx, batch, output, batch_loss in self.iter_batches():
+            pooler_output = output["pooler_output"]  # tensor in device
+            if isinstance(pooler_output, torch.Tensor):
+                pooler_output = move_to_device(
+                    pooler_output.detach(), device="cpu"
+                )
+            encoding = concat((encoding, pooler_output))
+            del pooler_output
+
+        if encoding is None:
+            msg = "unexpected output from model, encoding is None"
+            raise ValueError(msg)
+
+        return encoding
