6293f3a92ae5b82d81a144eb26ee6a001c9ed983
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-12 15:07:24 -0500

    Add caching for stopwords retrieval to improve performance

--- src/tklearn/kb/helpers.py
+++ src/tklearn/kb/helpers.py
@@ -1,10 +1,20 @@
 from __future__ import annotations
 
+import functools
 import re
+
+import nltk
 
 __all__ = [
     "codec",
 ]
+
+
+@functools.lru_cache(maxsize=1)
+def get_stopwords(language: str = "english") -> set[str]:
+    if language == "en":
+        return get_stopwords("english")
+    return set(nltk.corpus.stopwords.words(language))
 
 
 class Escaper:
