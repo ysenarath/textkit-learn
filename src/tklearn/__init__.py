9accd402e59ecb228444a857e01ab615a57c57fc
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-12 15:08:19 -0500

    Add basic setup function to configure SSL and suppress warnings

--- src/tklearn/__init__.py
+++ src/tklearn/__init__.py
@@ -1,4 +1,28 @@
+import ssl
+import warnings
+
 from tklearn.config import config
+
+
+def basic_setup():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+    # Suppress all Pydantic deprecation warnings coming from libraries like MLflow
+    warnings.filterwarnings("ignore", module="pydantic")
+    warnings.filterwarnings("ignore", module="mlflow")
+    # Suppress the specific regex escape warning
+    warnings.filterwarnings(
+        "ignore",
+        category=DeprecationWarning,
+        message="invalid escape sequence",
+    )
+
+
+basic_setup()
 
 __version__ = "0.4.0"
 
