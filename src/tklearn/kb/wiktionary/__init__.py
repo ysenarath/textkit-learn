590299f09463bbf324e07aca64d83df44c5a71dd
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-13 03:34:44 -0500

    Update __all__ in wiktionary module to include new line formatting

--- src/tklearn/kb/wiktionary/__init__.py
+++ src/tklearn/kb/wiktionary/__init__.py
@@ -1,4 +1,7 @@
 from tklearn.kb.wiktionary.store import WiktionaryArtifactStoreV1
 from tklearn.kb.wiktionary.store_v2 import WiktionaryArtifactStore
 
-__all__ = ["WiktionaryArtifactStore", "WiktionaryArtifactStoreV1"]
+__all__ = [
+    "WiktionaryArtifactStore",
+    "WiktionaryArtifactStoreV1",
+]
