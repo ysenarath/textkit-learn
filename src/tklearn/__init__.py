a290fe433e0e74a5a953a0089c635f062bd27a94
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-23 12:13:20 -0500

    Refactor SSL context setup by removing deprecated warning suppression

--- src/tklearn/__init__.py
+++ src/tklearn/__init__.py
@@ -1,30 +1,6 @@
 import ssl
-import warnings
-from contextlib import suppress
-
-import pydantic
 
 from tklearn.config import config
-
-
-def basic_setup():
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
-    with suppress(AttributeError):
-        warnings.filterwarnings(
-            "ignore", category=pydantic.warnings.PydanticDeprecatedSince212
-        )
-    with suppress(AttributeError):
-        warnings.filterwarnings(
-            "ignore", category=pydantic.warnings.PydanticDeprecatedSince20
-        )
-
-
-basic_setup()
 
 __version__ = "0.4.0"
 
@@ -32,3 +8,10 @@ __all__ = [
     "__version__",
     "config",
 ]
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
